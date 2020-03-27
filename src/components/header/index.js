ee49c810eccc9b43ee02affe03097d42eaf8a23f
amulyk <amulyk@lectrum.io>
2020-03-27 15:56:32 +0200

    add files

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './styles.module.css';
+import styles from './styles.css';
 
 export const Header = ({ title }) => (
   <header className={styles.content}>
